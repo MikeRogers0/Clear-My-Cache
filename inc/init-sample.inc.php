8fa5839b41ecfb8169098a77b682059506d4f69f
Mike Rogers <mike.r@fullondesign.co.uk>
2012-05-05 22:58:50 +0100

    making sample file and updating git ignore

---
+++ inc/init-sample.inc.php
@@ -0,0 +1,29 @@
+<?php
+// Start Time, used for performance testing.
+define('started', microtime());
+
+// Max out error reporting.
+error_reporting(-1);
+
+// Define Database Variables
+define('db_host', 'localhost');
+define('db_database', 'cmcdotme');
+define('db_database_salt', 'j6fm');
+define('db_username', 'root');
+define('db_password', '');
+
+// Cache information
+define('cache_dir', '/Users/MikeRogers/Sites/clearmycache.me/cache');
+
+// Start Sessions
+//session_name();
+//session_start();
+
+// Set the Timezone
+date_default_timezone_set('Europe/London');
+
+// Include all the required files.
+require('browscap.class.php');
+require('user_agent.class.php');
+require('db.class.php');
+?>

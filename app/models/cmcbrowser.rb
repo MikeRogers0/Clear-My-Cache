715c802789a165d6ec529464c1c52d693398d4ca
Mike Rogers <mike.r@fullondesign.co.uk>
2013-07-28 19:42:56 +0100

    adding uniqueness test and passing it

--- app/models/cmcbrowser.rb
+++ app/models/cmcbrowser.rb
@@ -1,3 +1,5 @@
 class Cmcbrowser < ActiveRecord::Base
 	belongs_to :cmcplatform
+
+	validates_uniqueness_of :name, :scope => [:slug, :version, :cmcplatform_id]
 end
--- test/models/cmcbrowser_test.rb
+++ test/models/cmcbrowser_test.rb
@@ -4,4 +4,13 @@ class CmcbrowserTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+
+  test "Can only add browser which has unique name, slug, version and platform" do
+  	browser = cmcbrowsers(:one).clone
+
+  	assert !browser.save, "Can't save identical browser"
+
+  	browser.version = (browser.version + 1)
+  	assert browser.save, "can save unique browser"
+  end
 end

d6b6c10242c003ea005986eb71f83ee60ea1c578
Mike Rogers <mike.r@fullondesign.co.uk>
2013-07-28 19:59:00 +0100

    adding a few validates model

--- app/models/cmcbrowser.rb
+++ app/models/cmcbrowser.rb
@@ -1,5 +1,18 @@
 class Cmcbrowser < ActiveRecord::Base
 	belongs_to :cmcplatform
 
+	#validates :name, :presence => true, :format => {:with => /^[a-z ]+$/i, :message => 'Must only contain letters and spaces'}
+
+	validates :slug, :presence => true, 
+                     :format => {
+                     	:with => /\A[a-z\-_]+\z/,
+                     	:message => 'Must only be lowercase letters, hyphens and underscores'
+                     }
+    validates :name, :presence => true, 
+                     :format => {
+                     	:with => /\A[a-z\- ]+\z/i,
+                     	:message => 'Must only be letters, spaces and hyphens'
+                     }
+
 	validates_uniqueness_of :name, :scope => [:slug, :version, :cmcplatform_id]
 end
--- test/models/cmcbrowser_test.rb
+++ test/models/cmcbrowser_test.rb
@@ -13,4 +13,26 @@ class CmcbrowserTest < ActiveSupport::TestCase
   	browser.version = (browser.version + 1)
   	assert browser.save, "can save unique browser"
   end
+
+  test "Only allow slug to be lowercase letters and hyphens" do
+  	browser = cmcbrowsers(:one).clone
+  	browser.slug = "this will Fai!"
+
+  	assert !browser.save
+  	assert browser.errors[:slug].include?('Must only be lowercase letters, hyphens and underscores'), 'Slug was formatted incorrectly'
+
+  	browser.slug = "this_this-fine"
+  	assert browser.save
+  end
+
+  test "Only allow name to be letters, spaces and hyphens" do
+  	browser = cmcbrowsers(:one).clone
+  	browser.name = "Chrome! "
+
+  	assert !browser.save
+  	assert browser.errors[:name].include?('Must only be letters, spaces and hyphens'), 'Name was formatted incorrectly'
+
+  	browser.name = "Google Chrome"
+  	assert browser.save
+  end
 end
